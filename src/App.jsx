d05fea6003609cf553c123fbd531a779c4015956
Matti Luukkainen <mluukkai@iki.fi>
2023-08-23 14:13:18 +0300

    part6-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import { useQuery } from '@tanstack/react-query'
-import { getNotes } from './requests' 
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getNotes, createNote, updateNote } from './requests' 
 
 const App = () => {
+  const queryClient = useQueryClient()
+  const newNoteMutation = useMutation({
+    mutationFn: createNote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['notes'] })
+    }
+  })
+
+  const updateNoteMutation = useMutation(updateNote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['notes'] })
+    },
+  })
+
   const addNote = async (event) => {
     event.preventDefault()
     const content = event.target.note.value
     event.target.note.value = ''
-    console.log(content)
+    newNoteMutation.mutate({ content, important: true })
   }
 
   const toggleImportance = (note) => {
-    console.log('toggle importance of', note.id)
+    updateNoteMutation.mutate({...note, important: !note.important })
   }
 
   const result = useQuery({
--- src/requests.js
+++ src/requests.js
@@ -1,4 +1,12 @@
 import axios from 'axios'
 
+const baseUrl = 'http://localhost:3001/notes'
+
 export const getNotes = () =>
-  axios.get('http://localhost:3001/notes').then(res => res.data)
+  axios.get(baseUrl).then(res => res.data)
+
+export const createNote = newNote =>
+  axios.post(baseUrl, newNote).then(res => res.data)
+
+export const updateNote = updatedNote =>
+  axios.put(`${baseUrl}/${updatedNote.id}`, updatedNote).then(res => res.data)
