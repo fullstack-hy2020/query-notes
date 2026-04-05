be88afa40fe1aa9587668b2e94eea26290017ce3
Matti Luukkainen <mluukkai@iki.fi>
2026-04-05 11:12:07 +0300

    fix2

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,9 @@ const App = () => {
       {notes.map((note) => (
         <li key={note.id} onClick={() => toggleImportance(note)}>
           {note.important ? <strong>{note.content}</strong> : note.content}
+          <button onClick={() => toggleImportance(note.id)}>
+            {note.important ? 'make not important' : 'make important'}
+          </button>       
         </li>
       ))}
     </div>
